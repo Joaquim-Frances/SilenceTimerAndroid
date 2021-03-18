357fc238b961cc54adab673719ad02229ab12705
Quim <quimifrances@gmail.com>
2021-03-18 11:42:52 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <WebView
+            automaticallyAdjustContentInsets={false}
+            source={{uri: 'https:silencetimer.com'}}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            startInLoadingState={true}
+            style={{marginTop: 25}}
+          /> 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+<StatusBar style='dark' backgroundColor='#0000'/>
